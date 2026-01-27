ebab2d2a3924ae2fff31f081e855a1608a7ca552
Dennesssy <139027232+Dennesssy@users.noreply.github.com>
2026-01-26 20:29:34 -0700

    Initial Commit

---
+++ OpenrouterTests/OpenrouterTests.swift
@@ -0,0 +1,16 @@
+//
+//  OpenrouterTests.swift
+//  OpenrouterTests
+//
+//  Created by Dennis Stewart Jr. on 1/26/26.
+//
+
+import Testing
+
+struct OpenrouterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
